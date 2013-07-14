f0e319f7db8b3586afeaa7597ddd4338ee8d62ed
Cesar Sandin <cesar.sandin@gmail.com>
2013-07-14 23:41:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fs = require('fs');
+    var inputfile = "index.html";
+    var buf new Buffer(fs.readFileSync(inputfile));
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
